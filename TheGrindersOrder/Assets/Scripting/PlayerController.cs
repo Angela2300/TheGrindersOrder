b1aaef936f464a4c0a565fc0b2b602d849f8a4e4
Angela Wong <celestialhearts05200615@gmail.com>
2026-06-17 15:24:17 +0800

    UpdatedPlayerCode&UIAssets

--- TheGrindersOrder/Assets/Scripting/PlayerController.cs
+++ TheGrindersOrder/Assets/Scripting/PlayerController.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // for Angela, I just need to create this first for MasterManager
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 move =
+            (Keyboard.current.dKey.isPressed ? Vector2.right : Vector2.zero) +
+            (Keyboard.current.aKey.isPressed ? Vector2.left : Vector2.zero) +
+            (Keyboard.current.wKey.isPressed ? Vector2.up : Vector2.zero) +
+            (Keyboard.current.sKey.isPressed ? Vector2.down : Vector2.zero);
+
+        rb.MovePosition(rb.position + move.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
 }
